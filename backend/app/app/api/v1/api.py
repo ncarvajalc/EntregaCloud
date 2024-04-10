f25f95a42152b19f691a552337b881ccf813498d
ernestocpc <88865467+ernestocpc@users.noreply.github.com>
2024-04-09 23:36:20 -0600

    Endpoint signup, login y JWT (#2)
    * Endpoint signup, login y JWT
    ---------
    
    Co-authored-by: Nicolás Carvajal <n.carvajalc@uniandes.edu.co>

--- backend/app/app/api/v1/api.py
+++ backend/app/app/api/v1/api.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import (
     weather,
+    auth,
 )
 
 api_router = APIRouter()
 
 # Include all routers
 api_router.include_router(weather.router, prefix="/weather", tags=["weather"])
+
+# /api/auth/signup
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
---
+++ backend/app/app/models/user.py
@@ -0,0 +1,11 @@
+from app.core.db import Base
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    password1 = Column(String, nullable=False)
